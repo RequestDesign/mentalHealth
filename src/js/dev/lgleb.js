96f43093f5d7d65082c3322ec4da786c1f0f6842
Gleb Leus <lv.gleb44@gmail.com>
2024-06-18 16:25:58 +0300

    activities, mission, questions blocks

--- src/js/dev/lgleb.js
+++ src/js/dev/lgleb.js
@@ -1,7 +1,16 @@
 import Swiper from 'swiper';
+import WOW from 'wow.js';
+
+import { rem } from '../utils/constants';
+
+import 'animate.css';
 
 window.addEventListener('DOMContentLoaded', () => {
   Swipers();
+  wowAnim();
+
+  document.querySelector('.accordion') && accordion();
+  document.querySelector('.mission') && missionLineAnimate();
 });
 
 const Swipers = () => {
@@ -25,4 +34,54 @@ const Swipers = () => {
       nextEl: '.activites__swiper-right-btn'
     }
   });
+};
+
+const wowAnim = () => {
+  const wow = new WOW({
+    boxClass: 'wow',
+    animateClass: 'animate__animated',
+    offset: 150,
+    mobile: true,
+    live: true
+  });
+  wow.init();
+};
+
+const accordion = () => {
+  const items = document.querySelectorAll('.accordion__item');
+
+  items.forEach((item) => {
+    const itemBody = item.querySelector('.accordion__item-body--wrapper'),
+      itemBodyInitialHeight = itemBody.scrollHeight,
+      itemBodyPadding = window.screen.width > 768 ? rem(4) : rem(9),
+      btn = item.querySelector('.accordion__item-switch');
+
+    itemBody.style.maxHeight = `0px`;
+
+    btn.addEventListener('click', () => {
+      item.classList.toggle('active');
+
+      item.classList.contains('active')
+        ? (itemBody.style.maxHeight = `${itemBodyInitialHeight + itemBodyPadding}px`)
+        : (itemBody.style.maxHeight = `0px`);
+    });
+  });
+};
+
+const missionLineAnimate = () => {
+  const line = document.querySelector('.mission__line');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add('active');
+        }, 500);
+      } else {
+        entry.target.classList.remove('active');
+      }
+    });
+  });
+
+  observer.observe(line);
 };
